011bd72778ba458632a55eb59019d69276a833df
bbrelin <bbrelin@gmail.com>
2023-04-18 04:22:45 +0300

    Updated translator.py file

--- translator.py
+++ translator.py
@@ -1,84 +1,82 @@
 import os
 import pandas as pd
 from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
 from transformers import MarianMTModel, MarianTokenizer
 import sys
 import glob
 import datetime
+from tqdm import tqdm
+from typing import List
+
 
 class Translator:
-    """
-    Translator class for translating text using MarianMT models.
-    """
+    def __init__(self):
+        pass
 
-    def __init__(self, models_root="models"):
+    def translate_batch(self, texts: List[str], source_language: str) -> List[str]:
+
         """
-        Initializes the Translator class.
+        Translates a batch of texts to English.
 
         Args:
-            models_root (str, optional): The root directory containing the translation models. Defaults to "models".
-        """
-        self.models_root = models_root
-
-    def translate_text(self, text, source_language):
-
-        """
-        Translates the given text to English.
-
-        Args:
-        text (str): The text to be translated.
-        source_language (str): The source language code.
+            texts (List[str]): A list of texts to be translated.
+            source_language (str): The source language code.
 
         Returns:
-        str: The translated text in English.
+            List[str]: A list of translated texts in English.
         """
 
         cache_dir = "/home/bbrelin/src/repos/newsletter/.cache"
 
         if source_language == 'en':
-            return text
+            return texts
         elif source_language in {'es', 'pt'}:
             model_name = "Helsinki-NLP/opus-mt-romance-en"
         else:
-            try:
-                model_name = f"Helsinki-NLP/opus-mt-{source_language}-en"
-                model = MarianMTModel.from_pretrained(model_name,cache_dir = cache_dir)
-                tokenizer = MarianTokenizer.from_pretrained(model_name)
-            except OSError:
-                model_name = "Helsinki-NLP/opus-mt-mul-en"
-                model = MarianMTModel.from_pretrained(model_name,cache_dir = cache_dir)
-                tokenizer = MarianTokenizer.from_pretrained(model_name)
+            model_name = f"Helsinki-NLP/opus-mt-{source_language}-en"
 
-            inputs = tokenizer(text, return_tensors="pt")
-            translated = model.generate(**inputs)
-            return tokenizer.decode(translated[0], skip_special_tokens=True)
+        try:
+            model = MarianMTModel.from_pretrained(model_name, cache_dir=cache_dir)
+            tokenizer = MarianTokenizer.from_pretrained(model_name)
+        except OSError:
+            model_name = "Helsinki-NLP/opus-mt-mul-en"
+            model = MarianMTModel.from_pretrained(model_name, cache_dir=cache_dir)
+            tokenizer = MarianTokenizer.from_pretrained(model_name)
 
+        inputs = tokenizer(texts, return_tensors="pt", padding=True, truncation=True, max_length=512)
+        translated = model.generate(**inputs)
+        return [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
 
-
-    def process_data(self, data):
-
+    def process_data(self, data: pd.DataFrame, batch_size: int = 10) -> pd.DataFrame:
         """
         Processes the input DataFrame by translating the 'Content' column to English.
 
         Args:
             data (pd.DataFrame): The input DataFrame with a 'Content' column.
-
+            batch_size (int, optional): The number of texts to be translated in a batch. Defaults to 10.
 
         Returns:
             pd.DataFrame: The output DataFrame with an additional 'translated_text' column containing the translated content in English.
         """
         data['translated_text'] = None
 
-        def safe_translation(text):
-           try:
-               lang = detect(text)
-               return self.translate_text(text, lang)
-           except Exception as e:
-               print(f"Error while translating text: {e}")
-            # Default to a specific model or skip translation
-               return text  # or self.translate_text(text, 'en')
+        def safe_translation(texts, source_language):
+            try:
+                return self.translate_batch(texts, source_language)
+            except Exception as e:
+                print(f"Error while translating text: {e}")
+                return texts
 
-        data['translated_text'] = data['Content'].apply(safe_translation)
+        texts = data['Content'].tolist()
+        translated_texts = []
+        for i in tqdm(range(0, len(texts), batch_size), desc="Translating"):
+            batch = texts[i:i + batch_size]
+            source_languages = [detect(text) for text in batch]
+            batch_translations = [safe_translation([text], lang) for text, lang in zip(batch, source_languages)]
+            translated_texts.extend(batch_translations)
+
+        data['translated_text'] = translated_texts
         return data
 
 def main(scraper_output_directory="/home/bbrelin/src/repos/newsletter/scraper_output"):
@@ -89,6 +87,7 @@ def main(scraper_output_directory="/home/bbrelin/src/repos/newsletter/scraper_ou
     Args:
         scraper_output_directory (str, optional): The directory containing the scraper output CSV files. Defaults to "/home/bbrelin/src/repos/newsletter/scraper_output".
     """
+
 
     list_of_files = glob.glob(os.path.join(scraper_output_directory, "scrape_results_*.csv"))
     latest_file = max(list_of_files, key=os.path.getmtime)
